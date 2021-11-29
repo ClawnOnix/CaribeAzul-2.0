47d1799af098443fc9b573aa2478f7c07ba71e19
Jorge Luis Lora <adrielramirez4@gmail.com>
2021-11-29 08:54:42 -0400

    Update Products.js

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -116,7 +116,7 @@ export default function Products(props) {
           <Input style={{ width: "50%", marginLeft: "10px" }} value={description} placeholder="Descripcion" Iprops={{ style: { height: "40px", borderRadius: "10px" } }} onChange={(e) => setDescription(e.target.value)} />
           <Input type="number" style={{ width: "30%", marginTop: "15px" }} value={price} placeholder="Precio" Iprops={{ style: { height: "40px", borderRadius: "10px" } }} onChange={(e) => setPrice(e.target.value)} />
           <Input type="number" style={{ width: "12%", marginLeft: "10px", marginTop: "15px" }} value={quantity} placeholder="cant." Iprops={{ style: { height: "40px", borderRadius: "10px" } }} onChange={(e) => setQuantity(e.target.value)} />
-          <input type="file" style={{ width: "198px", marginLeft: "1px", backgroundColor: "##00FF40", marginTop: "16px" }} value={file} onChange={onFileChange}></input>
+          <input type="file" style={{ width: "198px", marginLeft: "1px", backgroundColor: "##00FF40", marginTop: "16px" }} onChange={onFileChange}></input>
           <Button className="button" style={{ width: "20%", marginLeft: "10px" }} startIcon={<AddIcon />} onClick={() => { addProducts(name, description, price, quantity) }}>añadir producto</Button>
         </div>
       </Container>
